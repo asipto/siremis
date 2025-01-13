e7ada1ce24203077f3cce70ee7e733c1e4974ef9
Daniel-Constantin Mierla <miconda@gmail.com>
2025-01-13 17:20:53 +0100

    relocated functions related to session

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"crypto/md5"
-	"crypto/sha1"
-	"crypto/sha256"
 	"database/sql"
 	"encoding/hex"
 	"encoding/json"
@@ -97,8 +95,6 @@ type GMDBField struct {
 var GMConfigV = GMConfig{}
 
 var GMFuncMap = make(map[string]any)
-
-var GMSessions = map[string]GMSession{}
 
 var GMTemplatesV *template.Template = nil
 
@@ -741,29 +737,6 @@ func GMRemove(w http.ResponseWriter, r *http.Request, schemaName string, sId str
 	GMSMenuPage(w, r, schemaName)
 }
 
-func GMCheckPasswords(cfgPassword string, valPassword string) bool {
-	if strings.HasPrefix(cfgPassword, "sha256:") {
-		v := strings.TrimPrefix(cfgPassword, "sha256:")
-		hash := sha256.Sum256([]byte(v))
-		return (valPassword == hex.EncodeToString(hash[:]))
-	}
-	if strings.HasPrefix(cfgPassword, "sha1:") {
-		v := strings.TrimPrefix(cfgPassword, "sha1:")
-		hash := sha1.Sum([]byte(v))
-		return (valPassword == hex.EncodeToString(hash[:]))
-	}
-	if strings.HasPrefix(cfgPassword, "text:") {
-		return (valPassword == strings.TrimPrefix(cfgPassword, "text:"))
-	}
-	if strings.HasPrefix(cfgPassword, "md5:") {
-		v := strings.TrimPrefix(cfgPassword, "md5:")
-		hash := md5.Sum([]byte(v))
-		return (valPassword == hex.EncodeToString(hash[:]))
-	}
-
-	return (valPassword == cfgPassword)
-}
-
 func GMLoginCheck(w http.ResponseWriter, r *http.Request) int {
 	username := r.FormValue("username")
 	password := r.FormValue("password")
@@ -817,104 +790,6 @@ func GMLogin(w http.ResponseWriter, r *http.Request) {
 	viewData.Config = GMConfigV
 	viewData.Context.AuthOK = true
 	GMTemplatesV.ExecuteTemplate(w, "main", viewData)
-}
-
-func GMSessionAuthCheck(w http.ResponseWriter, r *http.Request) int {
-	c, err := r.Cookie("session_token")
-	if err != nil {
-		if err == http.ErrNoCookie {
-			// w.WriteHeader(http.StatusUnauthorized)
-			return -1
-		}
-		// w.WriteHeader(http.StatusBadRequest)
-		return -2
-	}
-	sessionToken := c.Value
-
-	userSession, exists := GMSessions[sessionToken]
-	if !exists {
-		// w.WriteHeader(http.StatusUnauthorized)
-		return -3
-	}
-	if userSession.IsExpired() {
-		delete(GMSessions, sessionToken)
-		// w.WriteHeader(http.StatusUnauthorized)
-		return -4
-	}
-	return 0
-}
-
-func GMSessionAuthActive(w http.ResponseWriter, r *http.Request) bool {
-	c, err := r.Cookie("session_token")
-	if err != nil {
-		if err == http.ErrNoCookie {
-			return false
-		}
-		return false
-	}
-	sessionToken := c.Value
-
-	userSession, exists := GMSessions[sessionToken]
-	if !exists {
-		return false
-	}
-	if userSession.IsExpired() {
-		delete(GMSessions, sessionToken)
-		return false
-	}
-	return true
-}
-
-func GMAuthRefresh(w http.ResponseWriter, r *http.Request) int {
-	c, err := r.Cookie("session_token")
-	if err != nil {
-		if err == http.ErrNoCookie {
-			// w.WriteHeader(http.StatusUnauthorized)
-			return -1
-		}
-		// w.WriteHeader(http.StatusBadRequest)
-		return -2
-	}
-	sessionToken := c.Value
-
-	userSession, exists := GMSessions[sessionToken]
-	if !exists {
-		// w.WriteHeader(http.StatusUnauthorized)
-		return -3
-	}
-	if userSession.IsExpired() {
-		log.Printf("session expired - token: %s\n", sessionToken)
-		delete(GMSessions, sessionToken)
-		// w.WriteHeader(http.StatusUnauthorized)
-		return -4
-	}
-
-	if userSession.expiry.After(time.Now().Add(-60 * time.Second)) {
-		// not yet close to expire
-		log.Printf("session active - token: %s\n", sessionToken)
-		return 1
-	}
-
-	newSessionToken := uuid.NewString()
-	log.Printf("new session token: %s\n", newSessionToken)
-
-	expiresAt := time.Now().Add(300 * time.Second)
-
-	GMSessions[newSessionToken] = GMSession{
-		username: userSession.username,
-		expiry:   expiresAt,
-	}
-
-	delete(GMSessions, sessionToken)
-
-	http.SetCookie(w, &http.Cookie{
-		Name:    "session_token",
-		Value:   newSessionToken,
-		Expires: expiresAt,
-		Path:    "/",
-	})
-
-	return 0
 }
 
 func GMLogout(w http.ResponseWriter, r *http.Request) int {
--- session.go
+++ session.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/hex"
+	"log"
+	"net/http"
+	"strings"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type GMSession struct {
@@ -11,4 +20,127 @@ type GMSession struct {
 
 func (s GMSession) IsExpired() bool {
 	return s.expiry.Before(time.Now())
+}
+
+var GMSessions = map[string]GMSession{}
+
+func GMCheckPasswords(cfgPassword string, valPassword string) bool {
+	if strings.HasPrefix(cfgPassword, "sha256:") {
+		v := strings.TrimPrefix(cfgPassword, "sha256:")
+		hash := sha256.Sum256([]byte(v))
+		return (valPassword == hex.EncodeToString(hash[:]))
+	}
+	if strings.HasPrefix(cfgPassword, "sha1:") {
+		v := strings.TrimPrefix(cfgPassword, "sha1:")
+		hash := sha1.Sum([]byte(v))
+		return (valPassword == hex.EncodeToString(hash[:]))
+	}
+	if strings.HasPrefix(cfgPassword, "text:") {
+		return (valPassword == strings.TrimPrefix(cfgPassword, "text:"))
+	}
+	if strings.HasPrefix(cfgPassword, "md5:") {
+		v := strings.TrimPrefix(cfgPassword, "md5:")
+		hash := md5.Sum([]byte(v))
+		return (valPassword == hex.EncodeToString(hash[:]))
+	}
+
+	return (valPassword == cfgPassword)
+}
+
+func GMSessionAuthCheck(w http.ResponseWriter, r *http.Request) int {
+	c, err := r.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			// w.WriteHeader(http.StatusUnauthorized)
+			return -1
+		}
+		// w.WriteHeader(http.StatusBadRequest)
+		return -2
+	}
+	sessionToken := c.Value
+
+	userSession, exists := GMSessions[sessionToken]
+	if !exists {
+		// w.WriteHeader(http.StatusUnauthorized)
+		return -3
+	}
+	if userSession.IsExpired() {
+		delete(GMSessions, sessionToken)
+		// w.WriteHeader(http.StatusUnauthorized)
+		return -4
+	}
+	return 0
+}
+
+func GMSessionAuthActive(w http.ResponseWriter, r *http.Request) bool {
+	c, err := r.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			return false
+		}
+		return false
+	}
+	sessionToken := c.Value
+
+	userSession, exists := GMSessions[sessionToken]
+	if !exists {
+		return false
+	}
+	if userSession.IsExpired() {
+		delete(GMSessions, sessionToken)
+		return false
+	}
+	return true
+}
+
+func GMAuthRefresh(w http.ResponseWriter, r *http.Request) int {
+	c, err := r.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			// w.WriteHeader(http.StatusUnauthorized)
+			return -1
+		}
+		// w.WriteHeader(http.StatusBadRequest)
+		return -2
+	}
+	sessionToken := c.Value
+
+	userSession, exists := GMSessions[sessionToken]
+	if !exists {
+		// w.WriteHeader(http.StatusUnauthorized)
+		return -3
+	}
+	if userSession.IsExpired() {
+		log.Printf("session expired - token: %s\n", sessionToken)
+		delete(GMSessions, sessionToken)
+		// w.WriteHeader(http.StatusUnauthorized)
+		return -4
+	}
+
+	if userSession.expiry.After(time.Now().Add(-60 * time.Second)) {
+		// not yet close to expire
+		log.Printf("session active - token: %s\n", sessionToken)
+		return 1
+	}
+
+	newSessionToken := uuid.NewString()
+	log.Printf("new session token: %s\n", newSessionToken)
+
+	expiresAt := time.Now().Add(300 * time.Second)
+
+	GMSessions[newSessionToken] = GMSession{
+		username: userSession.username,
+		expiry:   expiresAt,
+	}
+
+	delete(GMSessions, sessionToken)
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "session_token",
+		Value:   newSessionToken,
+		Expires: expiresAt,
+		Path:    "/",
+	})
+
+	return 0
 }
