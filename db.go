f7aab37b45629c7f72815a3864430d1eba75b9c4
Daniel-Constantin Mierla <miconda@gmail.com>
2025-01-13 19:43:18 +0100

    db.go: file to collect db-related functions

---
+++ db.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func dbConn() (db *sql.DB) {
+	log.Println("Database: " + GMConfigV.DBData.Database + " (" + GMConfigV.DBData.Host +
+		":" + GMConfigV.DBData.Port + ")")
+	db, err := sql.Open(GMConfigV.DBData.Driver, GMConfigV.DBData.Username+":"+
+		GMConfigV.DBData.Password+"@"+GMConfigV.DBData.Protocol+
+		"("+GMConfigV.DBData.Host+":"+GMConfigV.DBData.Port+")/"+
+		GMConfigV.DBData.Database)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
